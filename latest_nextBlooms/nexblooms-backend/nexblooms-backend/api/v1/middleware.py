312a19333fa9e6c7279a68d7ec9eadc8300dd6fc
Rupashree Roy <rupashreeroy2000@gmail.com>
2024-10-30 15:13:37 +0530

    delete backend

--- latest_nextBlooms/nexblooms-backend/nexblooms-backend/api/v1/middleware.py
+++
@@ -1,8 +0,0 @@
-# myapp/middleware.py
-
-from django.utils.deprecation import MiddlewareMixin
-
-class CustomCSPMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        response['Content-Security-Policy'] = "script-src 'self' chrome-extension:;"
-        return response
